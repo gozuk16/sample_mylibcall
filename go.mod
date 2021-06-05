59ff53a113a91fe1650499a27e55f9e1f4f8af56
GOZU Kenichiro <gozuk16@gmail.com>
2021-06-05 15:57:55 +0900

    プロセス情報を取得(並列と比較)。実施時間の計測

--- go.mod
+++ go.mod
@@ -2,6 +2,10 @@ module github.com/gozuk16/sample_mylibcall
 
 go 1.16
 
-require github.com/gozuk16/goss v0.0.0-00010101000000-000000000000
+require (
+	github.com/gozuk16/goss v0.0.0-00010101000000-000000000000
+	github.com/shirou/gopsutil/v3 v3.21.4
+	github.com/spf13/nitro v0.0.0-20131003134307-24d7ef30a12d
+)
 
 replace github.com/gozuk16/goss => ../goss
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -11,19 +12,31 @@ import (
 	"github.com/gozuk16/goss"
 	"github.com/gozuk16/goss/file"
 	"github.com/shirou/gopsutil/v3/process"
+	"github.com/spf13/nitro"
 )
 
 func main() {
+	timer := nitro.Initialize()
+	flag.Parse()
+	timer.Step("init")
+
 	fmt.Println(string(goss.Disk()))
+	timer.Step("server stats: disk")
 	fmt.Println(string(goss.Mem()))
+	timer.Step("server stats: mem")
 	fmt.Println(string(goss.Cpu()))
+	timer.Step("server stats: cpu")
 	fmt.Println(string(goss.Load()))
+	timer.Step("server stats: load")
 	fmt.Println(string(goss.Info()))
+	timer.Step("server stats: info")
+
 	//d, err := file.CrawlDirs("/Users/gozu/go/src/github.com/gozuk16", "go.mod")
 	d, err := file.CrawlDirs("service", "service.json")
 	if err != nil {
 		fmt.Println(err)
 	}
+	timer.Step("CrawlDirs")
 
 	var wg sync.WaitGroup
 	for _, v := range d {
@@ -34,16 +47,32 @@ func main() {
 		}(v)
 	}
 	wg.Wait()
+	timer.Step("get pid with goroutine")
 
+	for _, v := range d {
+		fmt.Println(string(goss.Process(getPid(v))))
+	}
+	timer.Step("get process")
+
+	var wg2 sync.WaitGroup
+	for _, v := range d {
+		wg2.Add(1)
+		go func(v string) {
+			defer wg2.Done()
+			fmt.Println(string(goss.Process(getPid(v))))
+		}(v)
+	}
+	wg2.Wait()
+	timer.Step("get process with goroutine")
 }
 
-func getPid(service string) {
+func getPid(service string) int32 {
 	f, err := os.Open(filepath.Join("/var/run", service+".pid"))
 	defer f.Close()
 	if err != nil {
 		fmt.Println(err)
 		fmt.Printf("%v: %d\n", service, -1)
-		return
+		return -1
 	}
 	scanner := bufio.NewScanner(f)
 	var t string
@@ -53,16 +82,16 @@ func getPid(service string) {
 	if err := scanner.Err(); err != nil {
 		fmt.Println(err)
 		fmt.Printf("%v: %d\n", service, -1)
-		return
+		return -1
 	}
 
 	pidno, err := strconv.Atoi(t)
 	if err != nil {
 		fmt.Println(err)
 		fmt.Printf("%v: %d\n", service, -1)
-		return
+		return -1
 	}
 	isAlive, err := process.PidExists(int32(pidno))
 	fmt.Printf("%v: %d, %v\n", service, pidno, isAlive)
-	return
+	return int32(pidno)
 }
